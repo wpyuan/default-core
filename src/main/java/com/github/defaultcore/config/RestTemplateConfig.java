f4d663a0b7639811918bce1e148cbdfaf563fc2e
wangpeiyuan <490176245@qq.com>
2021-04-10 07:33:36 +0800

    添加 消息转换器

--- src/main/java/com/github/defaultcore/config/RestTemplateConfig.java
+++ src/main/java/com/github/defaultcore/config/RestTemplateConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -43,13 +44,13 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateProperty restTemplateProperty) {
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-        fastJsonHttpMessageConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON_UTF8, MediaType.parseMediaType("application/x-www-form-urlencoded; charset=UTF-8")));
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON_UTF8));
         RestTemplate restTemplate = new RestTemplateBuilder()
                 .errorHandler(new DefaultErrorHandler())
                 .interceptors(defaultClientHttpRequestInterceptor)
                 .setConnectTimeout(Duration.ofMillis(restTemplateProperty.getConnectTimeout()))
                 .setReadTimeout(Duration.ofMillis(restTemplateProperty.getReadTimeout()))
-                .messageConverters(new StringHttpMessageConverter(StandardCharsets.UTF_8), fastJsonHttpMessageConverter)
+                .messageConverters(new StringHttpMessageConverter(StandardCharsets.UTF_8), fastJsonHttpMessageConverter, new FormHttpMessageConverter())
                 .build();
         restTemplate.setRequestFactory(new BufferingClientHttpRequestFactory(new OkHttp3ClientHttpRequestFactory()));
 
