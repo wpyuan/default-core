951679cc448ea8c3142d13910390d7430d848693
wangpeiyuan <490176245@qq.com>
2021-04-18 16:52:35 +0800

    修复 加载bean异常问题

--- src/main/java/com/github/defaultcore/config/DefaultMapperScannerAutoConfiguration.java
+++ src/main/java/com/github/defaultcore/config/DefaultMapperScannerAutoConfiguration.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
 public class DefaultMapperScannerAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean
+    @ConditionalOnMissingBean(name = "defaultMapperScannerConfigurer")
     public MapperScannerConfigurer defaultMapperScannerConfigurer() {
         MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
         mapperScannerConfigurer.setBasePackage("com.github.defaultcore.mapper");
